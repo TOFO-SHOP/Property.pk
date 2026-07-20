57e2cf32b5080a78027179821a2c7fc21700ab5f
TOFO-SHOP <usat5094@gmail.com>
2026-07-21 01:03:15 +0500

    Update validator.js

--- assets/js/utils/validator.js
+++ assets/js/utils/validator.js
@@ -1,1 +1,23 @@
+function isEmail(value) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+}
 
+function isPhone(value) {
+  return /^[0-9]{10,15}$/.test(value);
+}
+
+function isStrongPassword(value) {
+  return value.length >= 8;
+}
+
+function isNotEmpty(value) {
+  return value !== null && value !== undefined && value.toString().trim() !== '';
+}
+
+function validateSignupForm(data) {
+  const errors = {};
+  if (!isNotEmpty(data.name)) errors.name = 'Name is required';
+  if (!isEmail(data.email)) errors.email = 'Valid email is required';
+  if (!isStrongPassword(data.password)) errors.password = 'Password must be at least 8 characters';
+  return { valid: Object.keys(errors).length === 0, errors };
+}
